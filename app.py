e355bbd6ee9a3969a431d9171a62c327575f5f05
Cosikowy <mr.cosik@gmail.com>
2020-03-06 16:26:31 +0100

    api for fetching done

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restplus import Api, Resource, Namespace
+from scraper import Scrapper
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+api = Api(app, doc='/docs/', title='Scraper API', description='Scraper Api')
+
+CORS(app, resources=r'/api/*')
+
+scraper = api.namespace('Scraper', path='/scraper')
+scraper.add_resource(Scrapper, '/scraper/<mode>/<path:url>')
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.run(host='0.0.0.0', port=5000, debug=True)
--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 import shutil
 import os
-
+from flask_restplus import Resource
 
 def download_image(image_link, image_name, path):
     response = requests.get(image_link, stream=True)
@@ -16,13 +16,15 @@ def download_image(image_link, image_name, path):
     del response
 
 
-def scrap_text(url, path, decoder='utf-8'):
-    url = "https://www.python.org/"
+def scrap_text(url, host, decoder='utf-8'):
+    
+    # ściąga całą zawartość strony przygotowaną wg formatu Markdown
+
     html = urllib.request.urlopen(url).read().decode(decoder)
 
     text = get_text(html)
 
-    with open(f'./downloaded/{path}/page_content.txt', 'w', encoding=decoder) as _file:
+    with open(f'./downloaded/{host}/page_content.txt', 'w', encoding=decoder) as _file:
         _file.write(text)
 
 
@@ -31,10 +33,10 @@ def scrap_images(url, host):
     # Wyszukuje wszystkie elementy <img>, czyli domyślny objekt obrazu w HTML
 
     response = requests.get(url)
-    
+
     soup = BeautifulSoup(response.text, "html.parser")
     images = soup.find_all("img")
-    
+
     image_links = []
 
     for image in images:
@@ -67,10 +69,33 @@ def create_folder(url):
         pass
     else:
         os.mkdir(f'./downloaded/{host}')
+    return host
 
-    # scrap_text(url, host)
-    # scrap_images(url, host)
 
-create_folder('http://google.com')
+class Scrapper(Resource):
+    def __init__(self, url):
+        self.url = url
 
-# scrap_images_and_text("https://pl.wikipedia.org/wiki/Python", 'pl.wikipedia.org')
+    def post(self, mode, url):
+
+        host = create_folder(url)
+        
+        if mode == 'images':
+            scrap_images(url, host)
+            return 'Images fetched', 200
+
+        elif mode == 'content':
+            scrap_text(url, host)
+            return 'Content Fetched', 200
+
+        elif mode == 'both':
+            scrap_text(url, host)
+            scrap_images(url, host)
+            return 'Page content and images fetched', 200
+
+        else:
+            return 'Wrong mode', 404
+
+
+    def get(self, mode, host):
+        pass
